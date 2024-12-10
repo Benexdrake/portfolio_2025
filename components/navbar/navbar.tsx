346cb47c324df81873237ee805ffde1138a8f8a3
BeneX Drake <benex@hotmail.de>
2024-12-10 08:01:04 +0100

    adding navbar

---
+++ components/navbar/navbar.tsx
@@ -0,0 +1,20 @@
+import Link from "next/link";
+
+import style from '@/styles/navbar.module.css'
+
+export default function Navbar()
+{
+    return (
+        <nav className={style.navbar_body}>
+            <div className={style.navbar_links}>
+                <ul>
+                    <li><Link href="/">Home</Link></li>
+                    <li><Link href="about">About</Link></li>
+                    <li><Link href="projects">Projects</Link></li>
+                    <li><Link href="contact">Contact</Link></li>
+                    <li><Link href="impressum">Impressum</Link></li>
+                </ul>
+            </div>
+    </nav>
+    )
+}
