68173580a62d733e9f764ff47397a7720ad14398
BeneX Drake <benex@hotmail.de>
2024-12-12 08:36:34 +0100

    add skill block

---
+++ components/skills/skillBlock.tsx
@@ -0,0 +1,32 @@
+import { getIcon, getStars } from "@/lib/helper";
+import { Skill } from "@/types/skill";
+
+import style from '@/style_modules/home/skill.module.css'
+
+export default function SkillBlock(props:any)
+{
+    let skills = props.skills as Skill[];
+    let title = props.title as string;
+
+
+    return (
+        <article key={title}>
+        <h3 className={style.header}>{title}</h3>
+        <br />
+        <div className={style.skill_block}>
+          {skills.map(x => {
+            
+            if (x.type.toLowerCase() === title.toLowerCase()) {
+              console.log("HI")
+              return (
+                <div key={x.title} className={style.skill}>
+                  <img src={getIcon(x.icon)} alt="" height={50} />
+                  <p>{getStars(x.stars)}</p>
+                </div>
+              )
+            }
+          })}
+        </div>
+      </article>
+    )
+}
